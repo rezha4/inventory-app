6dba50189653eac3df8d92dad1db111622f12a66
rezha4 <rezha.bahari@gmail.com>
2023-11-12 14:36:52 +0700

    need to add mongoose and secret .env

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,8 +1,21 @@
 const Item = require("../models/items");
+const Category = require("../models/categories");
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  res.send("Home page not implemented");
+  const [
+    itemCount,
+    categoryCount
+  ] = await Promise.all([
+    Item.countDocuments({}).exec(),
+    Category.countDocuments({}).exec(),
+  ]);
+
+  res.render("index", {
+    title: "Inventory Home",
+    item_count: itemCount,
+    category_count: categoryCount,
+  });
 });
 
 exports.item_list = asyncHandler(async (req, res, next) => {
--- routes/catalog.js
+++ routes/catalog.js
@@ -10,8 +10,15 @@ router.get("/items", item_controller.item_list);
 
 router.get("/items/:id", item_controller.item_detail);
 
-// GET /items/create
-// GET /items/create
-// GET /items/:id/delete
-// POST /items/:id/delete
-// /items/update
+// GET /items/create form
+// POST /items/create update db
+// GET /items/:id/delete ?
+// POST /items/:id/delete update db
+// GET /items/:id/update
+// POST /items/:id/update
+
+router.get("/categories", category_controller.category_list);
+
+router.get("/categories/:id", category_controller.category_detail);
+
+module.exports = router;
