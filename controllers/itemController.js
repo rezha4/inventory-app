47b611ead91c4a72e6b30d04c33742ca08645e80
rezha4 <rezha.bahari@gmail.com>
2023-11-13 18:18:01 +0700

    update, delete items create, update, delete category

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,12 +1,10 @@
 const Item = require("../models/items");
 const Category = require("../models/categories");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  const [
-    itemCount,
-    categoryCount
-  ] = await Promise.all([
+  const [itemCount, categoryCount] = await Promise.all([
     Item.countDocuments({}).exec(),
     Category.countDocuments({}).exec(),
   ]);
@@ -23,12 +21,66 @@ exports.item_list = asyncHandler(async (req, res, next) => {
   res.render("items", {
     title: "All Items",
     items: items,
-  })
+  });
 });
 
 exports.item_detail = asyncHandler(async (req, res, next) => {
   const item = await Item.findById(req.params.id).populate("category").exec();
   res.render("item_detail", {
     item: item,
+  });
+});
+
+exports.item_create_get = asyncHandler(async (req, res, next) => {
+  const category = await Category.find().exec();
+  res.render("item_form", {
+    title: "Create Item",
+    category: category
   })
 });
+
+exports.item_create_post = [
+  (req, res, next) => {
+    if (!(req.body.category instanceof Array)) {
+      if (typeof req.body.category === "undefined") req.body.category = [];
+      else req.body.category = new Array(req.body.category);
+    }
+    next();
+  },
+
+  body("name", "Name cannot be empty").trim().isLength({ min: 3 }).escape(),
+  body("description", "Description cannot be empty")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("category.*").escape(),
+  body("price", "Price can't be 0").trim().isNumeric().escape(),
+  body("stock", "Stock can't be 0").trim().isNumeric().escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      stock: req.body.stock,
+    });
+
+    if (!errors.isEmpty()) {
+      const category = await Category.find({}).exec();
+      res.render("item_form", {
+        title: "Create Item",
+        name: item.name,
+        description: item.description,
+        category: category,
+        price: item.price,
+        stock: item.stock,
+      });
+    } else {
+      await item.save();
+      res.redirect(item.id);
+    }
+  }),
+];
--- routes/catalog.js
+++ routes/catalog.js
@@ -8,14 +8,18 @@ router.get("/", item_controller.index);
 
 router.get("/items", item_controller.item_list);
 
-router.get("/items/:id", item_controller.item_detail);
-
 // GET /items/create form
+router.get("/items/create", item_controller.item_create_get);
+
+router.post("/items/create", item_controller.item_create_post)
+
 // POST /items/create update db
 // GET /items/:id/delete ?
 // POST /items/:id/delete update db
 // GET /items/:id/update
 // POST /items/:id/update
+
+router.get("/items/:id", item_controller.item_detail);
 
 router.get("/categories", category_controller.category_list);
 
